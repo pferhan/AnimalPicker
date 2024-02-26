84d76704dfa28eadf47e943c75662734c06849da
pferhan <hannah.pfersch@quinnipiac.edu>
2024-02-25 22:00:00 -0500

    added fish option

--- AnimalPicker.java
+++ AnimalPicker.java
@@ -8,9 +8,9 @@ public class AnimalPicker {
 
         String choice = "";
         
-        while(!choice.equals("dog") && !choice.equals("cat"))
+        while(!choice.equals("dog") && !choice.equals("cat") && !choice.equals("fish"))
         {
-            System.out.println("Would you like to see a cat or dog?");
+            System.out.println("Would you like to see a dog, cat, or fish?");
 
             choice = scan.nextLine().toLowerCase();
     
@@ -18,8 +18,10 @@ public class AnimalPicker {
                 System.out.println("    ___\n __/_  `.  .-\"\"\"-.\n \\_,` | \\-\'  /   )`-\')\n  \"\") `\"`    \\  ((`\"`\n ___Y  ,    .\'7 /|\n(_,___/...-` (_/_/ ");
             } else if (choice.equals("cat")) {
                 System.out.println("      |\\      _,,,---,,_\nZZZzz /,`.-\'`\'    -.  ;-;;,_\n     |,4-  ) )-,_. ,\\ (  `\'-\'\n    \'---\'\'(_/--\'  `-\'\\_) ");
+            } else if (choice.equals("fish")) {
+                System.out.println("               O  o\n          _\\_   o\n>(\'>   \\\\/  o\\ .\n       //\\___=\n          \'\'");
             } else {
-                System.out.println("Error: Invalid selection - Please select either dog or cat");
+                System.out.println("Error: Invalid selection - Please select either dog, cat, or fish");
             }
         }
         scan.close();
