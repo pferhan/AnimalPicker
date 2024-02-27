c433f42c971075546736c26be99024ea974b88d8
taylorlea43 <taylor.lea@quinnipiac.edu>
2024-02-27 11:08:10 -0500

    User can now choose horse option

--- AnimalPicker.java
+++ AnimalPicker.java
@@ -8,9 +8,9 @@ public class AnimalPicker {
 
         String choice = "";
         
-        while(!choice.equals("dog") && !choice.equals("cat") && !choice.equals("fish"))
+        while(!choice.equals("dog") && !choice.equals("cat") && !choice.equals("fish") && !choice.equals("horse"))
         {
-            System.out.println("Would you like to see a dog, cat, or fish?");
+            System.out.println("Would you like to see a dog, cat, fish, or horse?");
 
             choice = scan.nextLine().toLowerCase();
     
@@ -20,8 +20,10 @@ public class AnimalPicker {
                 System.out.println("      |\\      _,,,---,,_\nZZZzz /,`.-\'`\'    -.  ;-;;,_\n     |,4-  ) )-,_. ,\\ (  `\'-\'\n    \'---\'\'(_/--\'  `-\'\\_) ");
             } else if (choice.equals("fish")) {
                 System.out.println("               O  o\n          _\\_   o\n>(\'>   \\\\/  o\\ .\n       //\\___=\n          \'\'");
+            } else if (choice.equals("horse")) {
+                System.out.println("                                 |\\    /|\n                              ___| \\,,/_/\n                           ---__/ \\/    \\\n                          __--/     (D)  \\\n                          _ -/    (_      \\\n                         // /       \\_ /  -\\\n   __-------_____--___--/           / \\_ O o)\n  /                                 /   \\__/\n /                                 /\n||          )                   \\_/\\\n||         /              _      /  |\n| |      /--______      ___\\    /\\  :\n| /   __-  - _/   ------    |  |   \\ \\\n |   -  -   /                | |     \\ )\n |  |   -  |                 | )     | |\n  | |    | |                 | |    | |\n  | |    < |                 | |   |_/\n  < |    /__\\                <  \\\n  /__\\                       /___\\");
             } else {
-                System.out.println("Error: Invalid selection - Please select either dog, cat, or fish");
+                System.out.println("Error: Invalid selection - Please select either dog, cat, fish, or horse");
             }
         }
         scan.close();
